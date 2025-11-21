acea980451a85748158aef7bb5b2fd693b326f5e
greencode-dev <antonioverde.dev@gmail.com>
2025-11-21 21:07:04 +0100

    snack2 completed

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,23 @@
 const people = [
   { name: 'Paolo', age: 35 },
   { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: 'Marco', age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
+
+// Stampa in pagina tutti i nomi
+// Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const peopleList = document.getElementById('output');
+
+for (let i = 0; i < people.length; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent = people[i].name;
+  peopleList.appendChild(listItem);
+}
