7fd8cd7fbae7416d766f0ccb0609b386b98e6e60
greencode-dev <antonioverde.dev@gmail.com>
2025-11-21 21:06:48 +0100

    snack1 completed

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,18 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+// Stampa in pagina tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+const namesList = document.getElementById('output');
+
+for (let i = 0; i < names.length; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent = names[i];
+  namesList.appendChild(listItem);
+}
